5cb691b835db10ad1120f2e5a2bf8a9e8178ebd7
Arthur Henrique Schlikmann <arthursh@outlook.com.br>
2023-06-25 16:08:20 -0300

    v13

--- textos.js
+++ textos.js
@@ -1,16 +1,23 @@
-function calculadora()
+function calculadora(){
 var n1;
 var n2;
 var menu;
-document.writeln("CALCULADORA INTERATIVA v-web(beta)");
+alert("CALCULADORA INTERATIVA v-web(beta)");
 menu = prompt("[1] para subtração, [2] para multiplicação ou [3] para divisão (OBS: A calculadora interativa verção web esta em desenvolvimento!)");
 n1 = prompt("Digite o primeiro valor");
 n2 = prompt("Digite o segundo valor");
 if(menu == 1) {
-    document.write("O resultado é ");
-    document.write(n1 - n2);
+    alert("O resultado é ");
+    alert(n1 - n2);
 }
 if(menu == 2) {
-    document.write("O resultado é ");
-    document.write(n1 * n2);
+    alert("O resultado é ");
+    alert(n1 * n2);
 }
+if(menu == 3) {
+    alert("O resultado é ");
+    alert(n1 / n2);
+}
+
+}
+
